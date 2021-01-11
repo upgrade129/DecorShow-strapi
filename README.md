90b8ecbdeba328d4c42f31fd5ba902aa6562e879
upgrade129 <premsanthosh129@outlook.com>
2021-01-11 19:09:31 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# DecorShow-strapi
+# Strapi application
+
+A quick description of your strapi application
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ef181e72-08b6-4242-bc61-8144fd3c3cb1'
+};
